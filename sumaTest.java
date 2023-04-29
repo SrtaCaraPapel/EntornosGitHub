c473f7951eb898bf63d2ddf927a9f04527092f9c
Atato21 <131300469+Atato21@users.noreply.github.com>
2023-04-29 10:19:10 +0200

    Add files via upload

---
+++ sumaTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class sumaTest {
+
+	@Test
+	public void sumaPositivos() {
+		System.out.println("Sumando dos numeros positivos...");
+		Suma S=new Suma(2,3);
+		assertTrue(S.sumar() == 5);
+	}
+
+}
